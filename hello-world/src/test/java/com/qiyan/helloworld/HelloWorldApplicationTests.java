6925fbf5e7f75a3f36cbe833e6044e39adcd6da6
qiyan <qiyanbeibei@163.com>
2020-01-08 19:52:50 +0800

    hello world

---
+++ hello-world/src/test/java/com/qiyan/helloworld/HelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qiyan.helloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
